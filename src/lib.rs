0e5be96593101d5c6c7a02cc02d28f16bd607607
mgr-inz-rafal <rchabowski@gmail.com>
2019-04-28 12:54:04 +0200

    Mutable static world state

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::sync::Mutex;
+
 const MAX_GAUCHOS: usize = 16;
 
 #[derive(Copy, Clone)]
@@ -11,40 +16,69 @@ struct World {
     gauchos: [Gaucho; MAX_GAUCHOS],
 }
 
-static WORLD: World = World {
-    gauchos: [Gaucho {
-        active: false,
-        x: 0.0,
-        y: 0.0,
-    }; MAX_GAUCHOS],
-};
+impl World {
+    fn reset(&mut self) {
+        self.clear_gauchos();
+    }
+    fn clear_gauchos(&mut self) {
+        self.gauchos.iter_mut().for_each(|mut x| x.active = false);
+    }
+}
 
-/*
-static world: World = World {
-    gauchos: [
-        Gaucho {
+lazy_static! {
+    static ref WORLD: Mutex<World> = Mutex::new(World {
+        gauchos: [Gaucho {
             active: false,
             x: 0.0,
             y: 0.0,
-        },
-        Gaucho {
-            active: false,
-            x: 0.0,
-            y: 0.0,
-        },
-        Gaucho {
-            active: false,
-            x: 0.0,
-            y: 0.0,
-        },
-    ],
-};
-*/
+        }; MAX_GAUCHOS],
+    });
+}
+
+fn count_gauchos() -> usize {
+    let mut counter = 0;
+    WORLD.lock().unwrap().gauchos.iter().for_each(|&x| {
+        if x.active {
+            counter += 1;
+        }
+    });
+    counter
+}
+
+fn find_exmpty_slot() -> Option<usize> {
+    WORLD
+        .lock()
+        .unwrap()
+        .gauchos
+        .iter()
+        .position(|&x| x.active == false)
+}
+
+fn add_gaucho() -> Result<(), &'static str> {
+    match find_exmpty_slot() {
+        None => Err("No more slots"),
+        Some(index) => {
+            WORLD.lock().unwrap().gauchos[index].active = true;
+            Ok(())
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn gauchos_count_zero() {
+        WORLD.lock().unwrap().reset();
+        assert_eq!(count_gauchos(), 0);
+    }
+
+    #[test]
+    fn add_gauchos() {
+        WORLD.lock().unwrap().reset();
+        let _ = add_gaucho();
+        let _ = add_gaucho();
+        assert_eq!(count_gauchos(), 2);
     }
 }
