7e745fb623f860de418e18472017697565e20818
mgr_inz_rafal <rchabowski@gmail.com>
2019-05-02 20:05:11 +0200

    Getting back to single file implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-pub mod carrier;
-pub mod tools;
-
-use carrier::Carrier;
-use tools::Position;
-
 #[macro_export]
 macro_rules! carrier {
     ($x: expr, $y: expr) => {
@@ -33,9 +27,23 @@ impl Slot {
     }
 }
 
+struct Dispatcher {}
+impl Dispatcher {
+    fn conduct(carriers: &mut Vec<Carrier>) {
+        let mut carrier = carriers[0];
+        match carrier.state {
+            _ => {}
+            State::IDLE => {
+                carrier.state = State::TARGETING((300.0, 300.0));
+            }
+        }
+    }
+}
+
 pub struct Swarm {
     carriers: Vec<Carrier>,
     slots: Vec<Slot>,
+    dispatcher: Dispatcher,
 }
 
 pub fn new() -> Swarm {
@@ -47,6 +55,7 @@ impl Swarm {
         Swarm {
             carriers: Vec::new(),
             slots: Vec::new(),
+            dispatcher: Dispatcher {},
         }
     }
 
@@ -71,7 +80,65 @@ impl Swarm {
     }
 
     pub fn tick(&mut self) {
+        Dispatcher::conduct(&mut self.carriers);
         self.carriers.iter_mut().for_each(|x| x.tick());
+    }
+}
+
+#[derive(Copy, Clone)]
+pub enum State {
+    IDLE,
+    TARGETING((f64, f64)),
+    MOVING,
+}
+
+#[derive(Copy, Clone)]
+pub struct Carrier {
+    pos: Position,
+    angle: f64,
+    state: State,
+}
+
+impl Carrier {
+    pub fn new(x: f64, y: f64) -> Carrier {
+        Carrier {
+            pos: Position::new(x, y),
+            angle: 0.0,
+            state: State::IDLE,
+        }
+    }
+
+    pub fn get_position(&self) -> &Position {
+        &self.pos
+    }
+
+    pub fn get_angle(&self) -> f64 {
+        self.angle
+    }
+
+    pub fn get_state(&self) -> State {
+        self.state
+    }
+
+    pub fn tick(&mut self) {
+        /*
+        self.pos.x = self.pos.x + self.angle.cos();
+        self.pos.y = self.pos.y + self.angle.sin();
+        self.angle += 0.01
+        */
+    }
+    
+}
+
+#[derive(Copy, Clone)]
+pub struct Position {
+    pub x: f64,
+    pub y: f64,
+}
+
+impl Position {
+    pub fn new(x: f64, y: f64) -> Position {
+        Position { x, y }
     }
 }
 
